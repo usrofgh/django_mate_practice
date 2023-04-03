bc277c796b563b292e1befd90fff4048036e4e4b
usrofgh <mailforgimail@gmail.com>
2023-04-03 10:35:53 +0300

    Realize STATIC_ROOT

--- app/urls.py
+++ app/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from app import settings
 
 urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path("admin/", admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
